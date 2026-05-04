083192cf6c94eaf4688a291c0dce5f9e565f54e6
ebrarkbenli <143252117+ebrarkbenli@users.noreply.github.com>
2026-05-04 19:29:48 +0300

    Configure SqLite and add initial migration

--- obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d89e7721cd2ee8b111111147a6134ebcdd63ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
